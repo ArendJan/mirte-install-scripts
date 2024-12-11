88b41e6fc2bd052be6d111f93eba95d9eeab22a3
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2024-12-11 12:06:06 +0100

    fix shellcheck

--- tools.sh
+++ tools.sh
@@ -5,7 +5,7 @@
 add_rc() {
 	lines=$1
 
-	grep -qxF "$1" ~/.bashrc || echo "$1" >>~/.bashrc
+	grep -qxF "$lines" ~/.bashrc || echo "$lines" >>~/.bashrc
 
 	# if we have a second argument, we use that as lines to add to zshrc, otherwise keep the same as bashrc
 	if [ -z "$2" ]; then
@@ -13,5 +13,5 @@ add_rc() {
 	else
 		lines=$2
 	fi
-	grep -qxF "$1" ~/.zshrc || echo "$1" >>~/.zshrc
+	grep -qxF "$lines" ~/.zshrc || echo "$lines" >>~/.zshrc
 }
