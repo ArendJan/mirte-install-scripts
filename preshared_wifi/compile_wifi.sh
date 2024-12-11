570b7a0f58ae2381d44cd07b4e621256ec312d15
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2024-12-11 10:25:37 +0100

    fix shfmt

--- preshared_wifi/compile_wifi.sh
+++ preshared_wifi/compile_wifi.sh
@@ -9,13 +9,13 @@ mkdir -p "$ssid"
 currdir=$(pwd)
 count=1
 for line in $(cat wifipassword.csv); do
-    mac=$(echo $line | cut -d, -f1)
-    password=$(echo $line | cut -d, -f2)
-    # use wpa_passphrase to get the hashed password
-    psk=$(wpa_passphrase "$ssid" "$password" | grep "psk" | grep -v "#" )
-    eval $psk
-    echo $psk > "$ssid/$count.txt"
-    openssl enc -e -aes256 -k "pass:$salt$mac" -in "$currdir/$ssid/$count.txt" -out "$currdir/$ssid/$count.enc" 
-    rm "$ssid/$count.txt"
-    count=$((count+1))
+	mac=$(echo $line | cut -d, -f1)
+	password=$(echo $line | cut -d, -f2)
+	# use wpa_passphrase to get the hashed password
+	psk=$(wpa_passphrase "$ssid" "$password" | grep "psk" | grep -v "#")
+	eval $psk
+	echo $psk >"$ssid/$count.txt"
+	openssl enc -e -aes256 -k "pass:$salt$mac" -in "$currdir/$ssid/$count.txt" -out "$currdir/$ssid/$count.enc"
+	rm "$ssid/$count.txt"
+	count=$((count + 1))
 done
--- preshared_wifi/setup_wifi.sh
+++ preshared_wifi/setup_wifi.sh
@@ -7,16 +7,16 @@ mac=$(ip addr show $(awk 'NR==3{print $1}' /proc/net/wireless | tr -d :) | awk '
 ssid="xxxxxx"
 # for file in $ssid/ try to decrypt
 for file in "$ssid/*.enc"; do
-    # decrypt file
-    openssl enc -d -aes256 -k "pass:$seed$mac" -in "$ssid/$file" -out "$ssid/found.txt" && found=true || true
-    if [ "$found" = true ]; then
-        # get password
-        password=$(cat "$ssid/found.txt")
-        echo "Password found: $password"
-        curr_netw=$(nmcli -t -f NAME c show --active)
+	# decrypt file
+	openssl enc -d -aes256 -k "pass:$seed$mac" -in "$ssid/$file" -out "$ssid/found.txt" && found=true || true
+	if [ "$found" = true ]; then
+		# get password
+		password=$(cat "$ssid/found.txt")
+		echo "Password found: $password"
+		curr_netw=$(nmcli -t -f NAME c show --active)
 
-        nmcli c down "$curr_netw" || true
-        sudo nmcli con add con-name "$ssid" type wifi ssid "$ssid" wifi-sec.key-mgmt wpa-psk wifi-sec.psk $password
-        break
-    fi
+		nmcli c down "$curr_netw" || true
+		sudo nmcli con add con-name "$ssid" type wifi ssid "$ssid" wifi-sec.key-mgmt wpa-psk wifi-sec.psk $password
+		break
+	fi
 done
