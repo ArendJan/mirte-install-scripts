00116a3d5532f9a852fa735528f2bfd59e625511
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-03-04 17:03:42 +0100

    Fix for zenoh mirte ros boot script

--- services/mirte_ros.sh
+++ services/mirte_ros.sh
@@ -2,21 +2,11 @@
 
 # by default use minimal launch file, but allow to override to minimal_master
 LAUNCH_FILE="${1:-minimal}"
+
+# If the robot user wants to add their own config:
+source /home/mirte/.profile
 source /home/mirte/mirte_ws/install/setup.bash
 
-# Not needed anymore, since we use the ROS_DOMAIN_ID
-# ip=$(hostname -I | awk '{print $1}') #just get the first ip addr
-# if [ "$(echo $ip | wc -w)" -ne 1 ]; then
-# 	# happens at boot, when the network is not yet up
-# 	echo "multiple or none, wont use the ip from hostname:"
-# 	hostname -I
-# else
-# 	export ROS_IP="$ip"
-# 	export ROS_MASTER_URI="http://$ip:11311/"
-# 	echo "ROS_IP=$ROS_IP"
-# 	echo "ROS_MASTER_URI=$ROS_MASTER_URI"
-# fi
-# If the robot user wants to add their own config:
+# .bashrc wont be sourced in this file, as its not an interactive shell.
 ros2 run rmw_zenoh_cpp rmw_zenohd &
-source /home/mirte/.bashrc
 ros2 launch mirte_bringup $LAUNCH_FILE.launch.py
