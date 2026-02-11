24132a1771153d8bc2513e912554a166bf320a70
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-02-11 17:19:34 +0100

    Fix cmake to old version

--- install_ROS.sh
+++ install_ROS.sh
@@ -12,7 +12,7 @@ chmod +x kitware-archive.sh
 sudo ./kitware-archive.sh
 rm kitware-archive.sh
 sudo apt update
-sudo apt install cmake -y
+sudo apt install cmake=3.31.5 -y
 # Install ROS Noetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
