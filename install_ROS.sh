02f4cdd6d8aab5938f7ac6053c29015b2dea2180
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-02-12 10:21:59 +0100

    fix cmake version install

--- install_ROS.sh
+++ install_ROS.sh
@@ -12,7 +12,9 @@ chmod +x kitware-archive.sh
 sudo ./kitware-archive.sh
 rm kitware-archive.sh
 sudo apt update
-sudo apt install cmake=3.31.5 -y
+sudo apt install cmake=3.31.5-0kitware1ubuntu20.04.1 cmake-data=3.31.5-0kitware1ubuntu20.04.1  -y --allow-downgrades
+# pin the version to prevent automatic updates that might break the build
+sudo apt-mark hold cmake cmake-data
 # Install ROS Noetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
